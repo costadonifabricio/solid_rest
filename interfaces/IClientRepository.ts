824ff561abb592d335107e3cecc180d683be1c8b
costadonifabricio <fabriciocostadoni@gmail.com>
2024-10-16 14:47:05 -0300

    feat: se crearon las interfaces y repositorys de vehicle y client

---
+++ interfaces/IClientRepository.ts
@@ -0,0 +1,7 @@
+import { IClient } from "./IClient";
+export interface IClientRepository {
+  create(client: IClient): Promise<IClient>;
+  findById(id: string): Promise<IClient | null>;
+  update(id: string, client: IClient): Promise<IClient | null>;
+  delete(id: string): Promise<void>;
+}
---
+++ interfaces/IVehicleRepository.ts
@@ -0,0 +1,8 @@
+import { IVehicle } from "./IVehicle";
+
+export interface IVehicleRepository {
+  create(vehicle: IVehicle): Promise<IVehicle>;
+  findById(id: string): Promise<IVehicle | null>;
+  update(id: string, vehicle: IVehicle): Promise<IVehicle | null>;
+  delete(id: string): Promise<void>;
+}
